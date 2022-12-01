f0e6cf5b52507aafa351b56c54f3608e72fa40c2
enrico <enrico.dsf@gmail.com>
2022-12-01 23:53:40 +0100

    super optimized day 1

--- erro/day01.py
+++ erro/day01.py
@@ -3,9 +3,9 @@ from itertools import groupby
 
 if __name__ == "__main__":
     with open(__file__.replace(".py", "_data")) as f:
-        data = f.readlines()
+        data = [r.strip() for r in f.readlines()]
 
-    calories_per_elf = sorted(sum(map(int, g)) for k, g in groupby(data, key=lambda x: x != "\n") if k)
+    calories_per_elf = sorted(sum(map(int, g)) for k, g in groupby(data, key=bool) if k)
 
     # PART 1
     print(calories_per_elf[-1])
