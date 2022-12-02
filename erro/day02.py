e8c888a4b3064634357c2a6ae3e88afe2f5c5f9c
enrico <enrico.dsf@gmail.com>
2022-12-03 00:54:25 +0100

    brutal solution for day2

--- erro/day02.py
+++ erro/day02.py
@@ -1,33 +1,9 @@
-STRATEGY_1 = {
-    "A X": 1 + 3,
-    "A Y": 2 + 6,
-    "A Z": 3 + 0,
-    "B X": 1 + 0,
-    "B Y": 2 + 3,
-    "B Z": 3 + 6,
-    "C X": 1 + 6,
-    "C Y": 2 + 0,
-    "C Z": 3 + 3
-}
-
-STRATEGY_2 = {
-    "A X": 3 + 0,
-    "A Y": 1 + 3,
-    "A Z": 2 + 6,
-    "B X": 1 + 0,
-    "B Y": 2 + 3,
-    "B Z": 3 + 6,
-    "C X": 2 + 0,
-    "C Y": 3 + 3,
-    "C Z": 1 + 6
-}
-
 if __name__ == "__main__":
     with open(__file__.replace(".py", "_data")) as f:
-        rounds = [line.strip() for line in f.readlines()]
+        rounds = [line.split() for line in f.readlines()]
 
     # PART 1
-    print(sum(STRATEGY_1[x] for x in rounds))
+    print(sum(ord(j) - ord("W") + (ord(j) - ord(i) - 1) % 3 * 3 for i, j in rounds))
 
     # PART 2
-    print(sum(STRATEGY_2[x] for x in rounds))
+    print(sum((ord(j) - 1) % 3 * 3 + (ord(j) + ord(i) - 1) % 3 + 1 for i, j in rounds))
