75f75a009eb23dea8d5b7c22f1f030f1f8122278
enrico <enrico.dsf@gmail.com>
2022-12-03 23:01:09 +0100

    nicer day3 part 1

--- erro/day03.py
+++ erro/day03.py
@@ -1,12 +1,8 @@
-from more_itertools import chunked
+with open(__file__.replace(".py", "_data")) as f:
+    data = [x.strip() for x in f.readlines()]
 
+# PART 1
+print(sum((y := ord((set(x[:len(x) // 2]) & set(x[len(x) // 2:])).pop())) - (96 if y > ord("Z") else 38) for x in data))
 
-if __name__ == "__main__":
-    with open(__file__.replace(".py", "_data")) as f:
-        data = [x.strip() for x in f.readlines()]
-
-    # PART 1
-    print(sum((y := ord(set.intersection(*map(set, chunked(x, len(x) // 2))).pop())) - (96 if y > ord("Z") else 38) for x in data))
-
-    # PART 2
-    print(sum((y := ord(set.intersection(*map(set, c)).pop())) - (96 if y > ord("Z") else 38) for c in chunked(data, 3)))
+# PART 2
+print(sum((y := ord(set.intersection(*map(set, data[3 * i: 3 * (i + 1)])).pop())) - (96 if y > ord("Z") else 38) for i in range(len(data) // 3)))
