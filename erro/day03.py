6f9e5eeaa5447c4897b0fe4f90955c9f29ebc7c6
enrico <enrico.dsf@gmail.com>
2022-12-03 16:21:45 +0100

    pure horror for day3

--- erro/day03.py
+++ erro/day03.py
@@ -1,17 +1,12 @@
 from more_itertools import chunked
 
 
-def get_priority(c):
-    item = set.intersection(*map(set, c)).pop()
-    return ord(item) - 96 if ord(item) > ord("Z") else ord(item) - 38
-
-
 if __name__ == "__main__":
     with open(__file__.replace(".py", "_data")) as f:
         data = [x.strip() for x in f.readlines()]
 
     # PART 1
-    print(sum(get_priority(chunked(x, len(x) // 2)) for x in data))
+    print(sum((y := ord(set.intersection(*map(set, chunked(x, len(x) // 2))).pop())) - (96 if y > ord("Z") else 38) for x in data))
 
     # PART 2
-    print(sum(get_priority(c) for c in chunked(data, 3)))
+    print(sum((y := ord(set.intersection(*map(set, c)).pop())) - (96 if y > ord("Z") else 38) for c in chunked(data, 3)))
