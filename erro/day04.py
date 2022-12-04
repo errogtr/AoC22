79ad3c51cb7f15c2ec1934cc2bc63482491d684b
enrico <enrico.dsf@gmail.com>
2022-12-04 19:11:17 +0100

    more succint day 4

--- erro/day04.py
+++ erro/day04.py
@@ -5,7 +5,7 @@ with open(__file__.replace(".py", "_data")) as f:
     sections = [list(map(int, re.findall(r"\d+", x))) for x in f.readlines()]
 
 # PART 1
-print(sum((a <= c and b >= d) or (c <= a and d >= b) for a, b, c, d in sections))
+print(sum((a <= c <= d <= b) or (c <= a <= b <= d) for a, b, c, d in sections))
 
 # PART 2
 print(sum(c <= b <= d or a <= d <= b for a, b, c, d in sections))
