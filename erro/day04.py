6015522977544f8e9b95003738158136fb78739d
enrico <enrico.dsf@gmail.com>
2022-12-04 11:04:32 +0100

    nicer day4 solution

--- erro/day04.py
+++ erro/day04.py
@@ -1,24 +1,15 @@
+def get_range(r):
+    start, end = map(int, r.split("-"))
+    return set(range(start, end + 1))
+
+
 with open(__file__.replace(".py", "_data")) as f:
     data = [x.split(",") for x in f.readlines()]
 
+sections = [[get_range(i) for i in x] for x in data]
+
 # PART 1
-counts = 0
-for range_1, range_2 in data:
-    section_1 = range_1.split("-")
-    section_2 = range_2.split("-")
-    set_1 = set(range(int(section_1[0]), int(section_1[1]) + 1))
-    set_2 = set(range(int(section_2[0]), int(section_2[1]) + 1))
-    if set_1 <= set_2 or set_2 <= set_1:
-        counts += 1
-print(counts)
+print(sum((x <= y) or (y <= x) for x, y in sections))
 
 # PART 2
-counts = 0
-for range_1, range_2 in data:
-    section_1 = range_1.split("-")
-    section_2 = range_2.split("-")
-    set_1 = set(range(int(section_1[0]), int(section_1[1]) + 1))
-    set_2 = set(range(int(section_2[0]), int(section_2[1]) + 1))
-    if set_1 & set_2:
-        counts += 1
-print(counts)
+print(sum(len(x & y) > 0 for x, y in sections))
