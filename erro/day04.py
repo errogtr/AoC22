4a3518985e0c2ac6f8de575654257b02ae5f18f1
enrico <enrico.dsf@gmail.com>
2022-12-04 11:40:20 +0100

    minor change in day4

--- erro/day04.py
+++ erro/day04.py
@@ -2,7 +2,7 @@ import re
 
 
 with open(__file__.replace(".py", "_data")) as f:
-    sections = [[int(y) for y in re.findall(r"\d+", x)] for x in f.readlines()]
+    sections = [list(map(int, re.findall(r"\d+", x))) for x in f.readlines()]
 
 # PART 1
 print(sum((a <= c and b >= d) or (c <= a and d >= b) for a, b, c, d in sections))
