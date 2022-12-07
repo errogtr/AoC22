3315ef99f8b686c17b0d7c400de9f1dd1c244d70
enrico <enrico.dsf@gmail.com>
2022-12-07 01:24:26 +0100

    using generators for day 6

--- erro/day06.py
+++ erro/day06.py
@@ -1,12 +1,6 @@
-def get_message(data: str, length: int):
-    for idx, c in enumerate(data[:-length + 1]):
-        if len(set(data[idx:idx + length])) == length:
-            return idx + length
-
-
 with open(__file__.replace(".py", "_data")) as f:
     data = f.read()
 
 # PART 1 & 2
-for length in [4, 14]:
-    print(get_message(data, length))
+for l in [4, 14]:
+    print(next(idx + l for idx, c in enumerate(data[:1 - l]) if len(set(data[idx:idx + l])) == l))
