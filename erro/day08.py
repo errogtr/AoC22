9a9b10d4c90759399112da29d2833ca661467fb7
enrico <enrico.dsf@gmail.com>
2022-12-13 22:55:45 +0100

    better looking day 8

--- erro/day08.py
+++ erro/day08.py
@@ -1,61 +1,42 @@
-from itertools import takewhile
+from math import prod
 
 
-def get_directions(heights, x, y):
-    up = [heights[(x, y - i)] for i in range(1, y + 1)]
-    down = [heights[(x, y + i)] for i in range(1, length - y)]
-    left = [heights[(x - i, y)] for i in range(1, x + 1)]
-    right = [heights[(x + i, y)] for i in range(1, length - x)]
+def get_directions(data, x, y):
+    up = [data[y - i][x] for i in range(1, y + 1)]
+    down = [data[y + i][x] for i in range(1, length - y)]
+    left = [data[y][x - i] for i in range(1, x + 1)]
+    right = [data[y][x + i] for i in range(1, length - x)]
     return up, down, left, right
+
+
+def get_view(height, direction):
+    direction_score = 0
+    for i in direction:
+        direction_score += 1
+        if i >= height:
+            break
+    return direction_score
 
 
 with open(__file__.replace(".py", "_data")) as f:
     data = f.read().splitlines()
 
-heights = dict()
-for y, line in enumerate(data):
-    for x, val in enumerate(line):
-        heights[(x, y)] = int(val)
-
 length = len(data)
 visible = 4 * (length - 1)
-for (x, y), height in heights.items():
-    if x in (0, length - 1) or y in (0, length - 1):
-        continue
+scenic_score = 0
+for y, row in enumerate(data):
+    for x, height in enumerate(row):
+        if x in (0, length - 1) or y in (0, length - 1):
+            continue
+        directions = get_directions(data, x, y)
 
-    for direction in get_directions(heights, x, y):
-        if max(direction) < height:
-            visible += 1
-            break
+        # PART 1
+        for d in directions:
+            if max(d) < height:
+                visible += 1
+                break
 
-print(visible)
+        # PART 2
+        scenic_score = max(scenic_score, prod(get_view(height, d) for d in directions))
 
-viewing_score = 0
-for (x, y), height in heights.items():
-    if x in (0, length - 1) or y in (0, length - 1):
-        continue
-
-    up, down, left, right = 0, 0, 0, 0
-    for i in range(1, y + 1):
-        up += 1
-        if heights[(x, y - i)] >= height:
-            break
-
-    for i in range(1, length - y):
-        down += 1
-        if heights[(x, y + i)] >= height:
-            break
-
-    for i in range(1, x + 1):
-        left += 1
-        if heights[(x - i, y)] >= height:
-            break
-
-    for i in range(1, length - x):
-        right += 1
-        if heights[(x + i, y)] >= height:
-            break
-
-    viewing_score = max(viewing_score, up * down * left * right)
-
-print(viewing_score)
+print(visible, scenic_score, sep="\n")
