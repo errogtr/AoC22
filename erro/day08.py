65f77749511abdc2ca867f2036f9fbf81b109eff
enrico <enrico.dsf@gmail.com>
2022-12-14 00:57:49 +0100

    better looking day 8

--- erro/day08.py
+++ erro/day08.py
@@ -1,21 +1,13 @@
 from math import prod
 
 
-def get_directions(data, x, y):
-    up = [data[y - i][x] for i in range(1, y + 1)]
-    down = [data[y + i][x] for i in range(1, length - y)]
-    left = [data[y][x - i] for i in range(1, x + 1)]
-    right = [data[y][x + i] for i in range(1, length - x)]
-    return up, down, left, right
-
-
 def get_view(height, direction):
-    direction_score = 0
+    score = 0
     for i in direction:
-        direction_score += 1
+        score += 1
         if i >= height:
             break
-    return direction_score
+    return score
 
 
 with open(__file__.replace(".py", "_data")) as f:
@@ -24,19 +16,20 @@ with open(__file__.replace(".py", "_data")) as f:
 length = len(data)
 visible = 4 * (length - 1)
 scenic_score = 0
-for y, row in enumerate(data):
-    for x, height in enumerate(row):
-        if x in (0, length - 1) or y in (0, length - 1):
-            continue
-        directions = get_directions(data, x, y)
+for y, row in enumerate(data[1:-1], 1):
+    for x, height in enumerate(row[1:-1], 1):
+        up = [data[y - i][x] for i in range(1, y + 1)]
+        down = [data[y + i][x] for i in range(1, length - y)]
+        left = [data[y][x - i] for i in range(1, x + 1)]
+        right = [data[y][x + i] for i in range(1, length - x)]
 
         # PART 1
-        for d in directions:
+        for d in (up, down, left, right):
             if max(d) < height:
                 visible += 1
                 break
 
         # PART 2
-        scenic_score = max(scenic_score, prod(get_view(height, d) for d in directions))
+        scenic_score = max(scenic_score, prod(get_view(height, d) for d in (up, down, left, right)))
 
 print(visible, scenic_score, sep="\n")
