fab23a49965dec33faad36e9ea6c001c5925b5f3
enrico <enrico.dsf@gmail.com>
2022-12-17 01:13:11 +0100

    day 10 part 2 solution

--- erro/day10.py
+++ erro/day10.py
@@ -1,24 +1,35 @@
 with open(__file__.replace(".py", "_data")) as f:
     data = f.read().splitlines()
 
-
-# PART 1
+# PART 1 & 2
 register = 1
 cycle = 1
 i = 0
 signal_strength = 0
+screen = ""
 first = True
 while i < len(data):
     if (cycle - 20) % 40 == 0:
         signal_strength += cycle * register
+
+    if register <= cycle % 40 <= register + 2:
+        screen += "#"
+    else:
+        screen += "."
+
     if data[i][:4] == "addx":
-        if first:
-            i -= 1
-        else:
+        if not first:
             register += int(data[i][4:])
+            i += 1
         first = not first
+    else:
+        i += 1
     cycle += 1
-    i += 1
 
 print(signal_strength)
 
+for pos, pixel in enumerate(screen, 1):
+    if pos % 40 > 0:
+        print(pixel, end="")
+    else:
+        print(pixel)
